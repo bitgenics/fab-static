5b0f5bae655760b10fd43cb4b8ddefdf3f06f2be
Erwin van der Koogh <hello@bitgenics.io>
2018-08-23 12:32:47 +1000

    add static file hosting to test

--- test.js
+++ test.js
@@ -1,7 +1,27 @@
+const fs = require('fs')
 const http = require('http')
+const url_parse = require('url').parse
+const mime = require('mime-types')
 
 const fab = require('./test/fab-dist/server/bundle.js')
 
-http.createServer((req, res) => {
-	fab.renderGet(req, res, {})
+const getContentType = pathname => {
+  const mimeType = mime.lookup(pathname)
+  return mimeType ? mime.contentType(mimeType) : 'text/html; charset=utf-8'
+}
+
+http.createServer(async (req, res) => {
+	const pathname = url_parse(req.url).pathname
+	if(pathname.startsWith('/_assets')) {
+		fs.readFile('./test/fab-dist' + pathname, (err, data) => {
+			res.setHeader('Content-Type', getContentType(pathname))
+			res.end(data)
+		})
+	} else {
+		await fab.renderGet(req, res, {})
+	}
+
+	
 }).listen(3005)
+
+console.log('ready')
